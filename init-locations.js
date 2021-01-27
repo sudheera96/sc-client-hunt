d35763e8d476b51af3ab2ebe63322654e5b5a39d
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2021-01-27 06:28:39 -0600

    added lat and long of two locations

---
+++ init-locations.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        Name: '1213 Apartments',
+        Latitude: 40.360465,
+        Longitude: -94.887566
+    },
+    {
+        Name: '1217 Apartments',
+        Latitude: 40.360970,
+        Longitude: -94.888242
+    }
+
+];
